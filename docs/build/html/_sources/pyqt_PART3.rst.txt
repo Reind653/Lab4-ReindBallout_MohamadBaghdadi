582d6e7ffe6efdc45f80b05cb4336066184136d4
Mohamad <mkb31@mail.aub.edu>
2024-10-19 13:04:58 +0300

    Add PyQt UI and Sphinx documentation

---
+++ docs/build/html/_sources/pyqt_PART3.rst.txt
@@ -0,0 +1,7 @@
+PyQt PART3 Module
+=================
+
+.. automodule:: pyqt_PART3
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PyQt GUI'
+copyright = '2024, Mohamad Baghdadi'
+author = 'Mohamad Baghdadi'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('/home/m/Desktop/AUB/E3/Fall 24-25/EECE 435L/lab2'))
+
